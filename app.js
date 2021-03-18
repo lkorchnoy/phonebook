192d88e568d1fd95d0c051136e4c42fac05f80f6
lkorchnoy <lkorchnoy@yahoo.com>
2021-03-18 12:54:12 -0400

    added removeContact method to the store

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ class Contact {
 
 class UI {
     static displayNewContacts() {
-        const StoreContacts = [
+        const contacts = Store.getContacts();
+        /* const StoreContacts = [
             {
                 name: 'Isabella',
                 phonenumber: '1234567891'
@@ -28,10 +29,10 @@ class UI {
                 name: 'Anastasia',
                 phonenumber: '1234567895'
             }
-        ];
+        ]; */
 
-        const contacts = StoreContacts;
-
+        /* const contacts = StoreContacts;
+ */
         contacts.forEach((contact) => UI.addContactToList(contact));
     }
     static addContactToList(contact) {
@@ -93,10 +94,16 @@ class Store {
     }
 
     static removeContact(name) {
-        
+        const contacts = Store.getContacts();
 
+        contacts.forEach((contact, index) => {
+            if(contact.name === name) {
+                contacts.splice(index, 1);
+            }
+        });
+
+        localStorage.setItem('contacts', JSON.stringify(contacts));
     }
-
 }
      //event: display contacts
 document.addEventListener('DOMContentLoaded', UI.displayNewContacts);
@@ -122,6 +129,9 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
     //add contact to ui
     UI.addContactToList(contact);
 
+    //add contact to local storage
+    Store.addContact(contact);
+
     //success message
     UI.showAlert('Contact Added', 'success');
 
@@ -132,7 +142,10 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
 });
 //remove a contact 
 document.querySelector('#name-list').addEventListener('click', (e) => {
-
+//remove contact from UI
     UI.deleteContact(e.target)
+//remove contact from localstorage
+    Store.removeContact(e.target.parentElement.previousElementSibling.textContent);
+//warning allert for removing contact 
     UI.showAlert('Contact Removed', 'warning');
 });
